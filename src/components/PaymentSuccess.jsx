a05f3aba7a2180ea8387cd1bccf5cb4411c80e0e
Abhinav <abhinavsd07@gmail.com>
2026-01-14 09:55:31 +0530

    Book store

--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 const PaymentSuccess = () => {
   return (
     <div>
+      <Header />
       <h1>Payment Successful</h1>
+      <div>
+        <img src="https://www.cntraveller.in/wp-content/themes/cntraveller/images/check-circle.gif" alt="" />
+      </div>
     </div>
   )
 }
